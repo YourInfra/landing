2b2cf7e1fc3ab5cd75a3b8dc7d8673aec56d9395
speckzzz <speckzzz@outlook.com>
2025-06-27 11:16:03 +0300

    Cleanup and adding YM

---
+++ _app/immutable/entry/start.48P3EmFv.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/R6qwnlS6.js";export{o as load_css,r as start};
--- _app/immutable/nodes/3.Bv_kjen6.js
+++
@@ -1,1 +0,0 @@
-import{f as r,a as p}from"../chunks/BahZ92aV.js";import"../chunks/CyhzAusp.js";var e=r('<a href="/demo/paraglide">paraglide</a>');function f(a){var o=e();p(a,o)}export{f as component};
