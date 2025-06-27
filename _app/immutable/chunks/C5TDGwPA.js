ac730cd4fe4ee6056f09a875887e760e1275c523
speckzzz <speckzzz@outlook.com>
2025-06-27 11:08:13 +0300

    Up

---
+++ _app/immutable/chunks/C5TDGwPA.js
@@ -0,0 +1,1 @@
+import{h as a}from"./1uW17Q1y.js";a();
---
+++ _app/immutable/nodes/3.D-v7-jcB.js
@@ -0,0 +1,1 @@
+import{f as r,a as p}from"../chunks/DYxHWidw.js";import"../chunks/C5TDGwPA.js";var e=r('<a href="/demo/paraglide">paraglide</a>');function f(a){var o=e();p(a,o)}export{f as component};
